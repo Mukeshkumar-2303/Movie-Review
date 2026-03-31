e3f9aa04ecb8a6609da96f49a0bc0864c4769ba9
Mukesh kumar T <mukeshkumart2305@gmail.com>
2026-03-31 14:10:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np  
-from tensorflow.keras.datasets import imdb 
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import sequence
-import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import sequence
 import pickle
